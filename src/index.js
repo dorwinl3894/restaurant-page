0717cf400f505d9466733737189338bb60c0b415
Dorwin Liang <dorwinliang@gmail.com>
2022-10-29 20:21:21 -0400

    development mode

--- src/index.js
+++ src/index.js
@@ -5,34 +5,27 @@ import {createGalleryContent} from './gallery';
 import {createContactContent} from './contact';
 
 
-createHomeContent();
-
 
 let homeButton = document.getElementById('home-button');
 let galleryButton = document.getElementById('gallery-button');
 let contactButton = document.getElementById('contact-button');
 
-
+createHomeContent();
 
 homeButton.addEventListener('click',() => {
     clearContent();
     createHomeContent();
-    console.log("Clicked");
-
-    
 })
 
 
 galleryButton.addEventListener('click',() => {
     clearContent();
     createGalleryContent();
-    console.log("Clicked");
 })
 
 
 contactButton.addEventListener('click',() => {
     clearContent();
     createContactContent();
-    console.log("Clicked");
 })
 
